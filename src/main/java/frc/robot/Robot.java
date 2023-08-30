51cbe92dd06d336b0a0f7ac9ce6f2534e1b41eed
useshubabu@gmail.com <useshubabu@gmail.com>
2023-08-29 20:48:00 -0400

    Added Team Colors command to turn on the lights

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.DriveTrainSubSystem;
+import frc.robot.subsystems.TeamColorDisplay;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -20,6 +21,7 @@ public class Robot extends TimedRobot {
 
   public static RobotContainer m_robotContainer;
   public static DriveTrainSubSystem myDriveTrain = new DriveTrainSubSystem();
+  public static TeamColorDisplay myTeamColorsDisplay = new TeamColorDisplay();
 
 
   /**
