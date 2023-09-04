8513b9344ba61eaa90f460762d8163a1fe35fd00
useshubabu@gmail.com <useshubabu@gmail.com>
2023-09-04 11:32:28 -0400

    Reduced the speed of the drivetrain

--- src/main/java/frc/robot/commands/SweeperCommand.java
+++ src/main/java/frc/robot/commands/SweeperCommand.java
@@ -92,6 +92,7 @@ public class SweeperCommand extends CommandBase {
   public boolean isFinished() {
     return false;
   }
+  //Not using this  method in sprint1
   int computeMotionConstraint(boolean state, int constraint, int lastDirection)
   {
     int newConstraint = SweeperCommand.FREE;
